9155b5622571c97b60ec16eb3eab0be677978bbf
wolwerr <ricardo@dtmm.com.br>
2023-11-01 23:33:40 -0300

    "Sending integration tests"

---
+++ src/test/java/com/example/host/controller/BlockControllerIntegrationTest.java
@@ -0,0 +1,106 @@
+package com.example.host.controller;
+
+import com.example.host.entities.Block;
+import com.example.host.repositories.BlockRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class BlockControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private BlockRepository blockRepository;
+
+    @Test
+    public void shouldReturnAllBlocks() throws Exception {
+        this.mockMvc.perform(get("/api/blocks"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldCreateBlock() throws Exception {
+        String jsonRequest = "{\"startDate\":\"2022-01-01\",\"endDate\":\"2022-01-10\",\"reason\":\"Maintenance\"}";
+
+        this.mockMvc.perform(post("/api/blocks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void shouldUpdateBlock() throws Exception {
+        String jsonRequest = "{\"startDate\":\"2022-01-05\",\"endDate\":\"2022-01-15\",\"reason\":\"Event\"}";
+
+        this.mockMvc.perform(put("/api/blocks/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldDeleteBlock() throws Exception {
+        this.mockMvc.perform(delete("/api/blocks/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldNotAllowBlockStartDateAfterEndDate() throws Exception {
+        String jsonRequest = "{\"startDate\":\"2022-01-10\",\"endDate\":\"2022-01-05\",\"reason\":\"Event\"}";
+
+        this.mockMvc.perform(post("/api/blocks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @Transactional
+    public void shouldSaveBlockToDatabase() throws Exception {
+        String jsonRequest = "{\"startDate\":\"2022-01-01\",\"endDate\":\"2022-01-10\",\"reason\":\"Maintenance\"}";
+
+        this.mockMvc.perform(post("/api/blocks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isCreated());
+
+        List<Block> blocks = blockRepository.findAll();
+        assertThat(blocks.get(0).getReason()).isEqualTo("Maintenance");
+    }
+
+    @Test
+    @Transactional
+    public void shouldNotAllowOverlappingDatesForBlock() throws Exception {
+        Block existingBlock = new Block();
+        existingBlock.setStartDate(LocalDate.of(2022, 1, 1));
+        existingBlock.setEndDate(LocalDate.of(2022, 1, 10));
+        existingBlock.setReason("Maintenance");
+        blockRepository.save(existingBlock);
+
+        String jsonRequest = "{\"startDate\":\"2022-01-05\",\"endDate\":\"2022-01-15\",\"reason\":\"Private Event\"}";
+
+        this.mockMvc.perform(post("/api/blocks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isConflict());
+
+    }
+}
---
+++ src/test/java/com/example/host/controller/BookingControllerIntegrationTest.java
@@ -0,0 +1,107 @@
+package com.example.host.controller;
+
+import com.example.host.entities.Booking;
+import com.example.host.repositories.BookingRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class BookingControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    @Test
+    public void shouldReturnAllBookings() throws Exception {
+        this.mockMvc.perform(get("/api/bookings"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldCreateBooking() throws Exception {
+        String jsonRequest = "{\"startDate\":\"2022-01-01\",\"endDate\":\"2022-01-10\",\"guestData\":\"John Doe\"}";
+
+        this.mockMvc.perform(post("/api/bookings")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void shouldUpdateBooking() throws Exception {
+        String jsonRequest = "{\"startDate\":\"2022-01-05\",\"endDate\":\"2022-01-15\",\"guestData\":\"Jane Doe\"}";
+
+        this.mockMvc.perform(put("/api/bookings/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldDeleteBooking() throws Exception {
+        this.mockMvc.perform(delete("/api/bookings/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldNotAllowStartDateAfterEndDate() throws Exception {
+        String jsonRequest = "{\"startDate\":\"2022-01-10\",\"endDate\":\"2022-01-05\",\"guestData\":\"John Doe\"}";
+
+        this.mockMvc.perform(post("/api/bookings")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @Transactional
+    public void shouldSaveBookingToDatabase() throws Exception {
+        String jsonRequest = "{\"startDate\":\"2022-01-01\",\"endDate\":\"2022-01-10\",\"guestData\":\"John Doe\"}";
+
+        this.mockMvc.perform(post("/api/bookings")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isCreated());
+
+        List<Booking> bookings = bookingRepository.findAll();
+        assertThat(bookings.get(0).getGuestData()).isEqualTo("Guest A");
+    }
+
+    @Test
+    @Transactional
+    public void shouldNotAllowOverlappingDates() throws Exception {
+        Booking existingBooking = new Booking();
+        existingBooking.setStartDate(LocalDate.of(2022, 1, 1));
+        existingBooking.setEndDate(LocalDate.of(2022, 1, 10));
+        existingBooking.setGuestData("Existing Guest");
+        bookingRepository.save(existingBooking);
+
+        String jsonRequest = "{\"startDate\":\"2022-01-05\",\"endDate\":\"2022-01-15\",\"guestData\":\"John Doe\"}";
+
+        this.mockMvc.perform(post("/api/bookings")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isConflict());
+
+    }
+
+
+}
